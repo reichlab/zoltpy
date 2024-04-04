e09238c76d234877db71874c45a0283636d57b48
matthewcornell <matt@matthewcornell.org>
2024-04-04 12:58:03 -0400

    [create new prediction_types for mean and median #367]: incorporated review comments

---
+++ cli/validate_quantile_csv_file.py
@@ -0,0 +1,19 @@
+import click
+
+from zoltpy.covid19 import validate_quantile_csv_file
+
+
+@click.command()
+@click.argument('quantile_csv_file', type=click.Path(file_okay=True, exists=True))
+def validate_quantile_csv_file_app(quantile_csv_file):
+    """
+    Simple CLI wrapper of `validate_quantile_csv_file()`
+
+    :param csv_fp: as passed to `json_io_dict_from_quantile_csv_file()`
+    :return:
+    """
+    print(validate_quantile_csv_file(quantile_csv_file))
+
+
+if __name__ == '__main__':
+    validate_quantile_csv_file_app()
